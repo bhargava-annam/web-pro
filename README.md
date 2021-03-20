f13b865d700dae2e88e8f32fcfbf065a1da1e876
Bhargava Annam <bhargava.annam@mountblue.tech>
2021-03-20 11:48:27 +0530

    Coding part completed

---
+++ README.md
@@ -0,0 +1,30 @@
+# data-project-web
+
+IPL data project. In this case the plotting happens on the browser with JS.
+
+# Data Files
+
+ * https://www.kaggle.com/manasgarg/ipl/version/5
+
+
+### In the above link there are two files
+
+* deliveries.csv
+* matches.csv
+
+# Steps to install all gems from gemfile
+
+* run the command in your shell
+   
+      bundle install
+
+# Execution
+ 
+* Open **index.html** in the browser.
+*  **json** folder containd all the json objects rewuired to plot.
+*  **script.js** cosist the plotting part of json objects.
+
+# Hosting
+
+* This project is hosted on herokuapp. Below is the link to access my data-web project.
+  
---
+++ script.js
@@ -0,0 +1,164 @@
+// Problem1
+fetch('./json/problem1.json')
+.then(function(response) {
+    return response.json();
+})
+.then(function (obj){
+    let dataArray = Object.entries(obj);
+    const chart1 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'problem1',
+            type: 'column',
+
+        },
+        title: {
+            text: 'Total runs scored by team'
+        },
+        xAxis:{
+            type: 'category'
+        },
+        yAxis: {
+            min: 0,
+            title: {
+              text: " Total runs "
+            }
+          },  
+        series: [{
+            name: 'Runs',
+            colorByPoint: true,
+            data: dataArray
+        }]
+    });
+})
+.catch(function(error){
+    console.log('Something went wrong');
+    console.error(error);
+});
+
+// Problem 2
+
+fetch('./json/problem2.json')
+.then(function(response) {
+    return response.json();
+})
+.then(function (obj){
+    let dataArray = Object.entries(obj);
+    const chart2 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'problem2',
+            type: 'column',
+    
+        },
+        title: {
+            text: 'Top 5 batsman of RCB'
+        },
+        xAxis:{
+            type: 'category'
+        },
+        yAxis: {
+            min: 0,
+            title: {
+              text: "Total runs"
+            }
+          },  
+        series: [{
+            name: 'Runs',
+            colorByPoint: true,
+            data: dataArray
+        }]
+    });
+})
+.catch(function(error){
+    console.log('Something went wrong');
+    console.error(error);
+});
+
+
+// Problem 3 starts
+
+fetch('./json/problem3.json')
+.then(function(response) {
+    return response.json();
+})
+.then(function (obj){
+    let dataArray = Object.entries(obj);
+    const chart3 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'problem3',
+            type: 'column',
+            },
+        
+        title: {
+            text: 'Foreign umpire analysis'
+        },
+        xAxis:{
+            type: 'category'
+
+        },
+        yAxis: {
+            min: 0,
+            title: {
+              text: "umpire count"
+            }
+          },  
+        series: [{
+            name: 'No of Umpires',
+            colorByPoint: true,
+            data: dataArray
+        }]
+    });
+})
+.catch(function(error){
+    console.log('Something went wrong');
+    console.error(error);
+});
+// Problem 3 ends 
+
+
+//problem4 starts
+fetch('./json/problem4.json')
+.then(function(response) {
+    return response.json();
+})
+.then(function (obj){
+    dataArray=[]
+    for(i in obj){
+        dataArray.push({name: i, data: obj[i]})
+    }
+    let season = ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
+    const chart4 = new Highcharts.chart('problem4', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Stacked-bar chart of matches played by team by season'
+        },
+        subtitle: {
+          text: 'Number of games played by team by season'
+        },
+        xAxis: {
+            categories:season,
+            crosshair: true
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Matches played'
+          }
+        },
+        tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.0f} </b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+        },
+        series: dataArray
+        
+      });
+})
+.catch(function(error){
+    console.log('Something went wrong');
+    console.error(error);
+});
